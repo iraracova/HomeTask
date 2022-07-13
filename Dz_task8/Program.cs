fddf34be1bf5150d6e11a70c0baf90ddc92065c1
Ira Rakova <racova-ir@mail.ru>
2022-07-13 22:23:22 +0300

    add task8

---
+++ Dz_task8/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число:");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+while(count <= N)
+{
+if(count % 2 == 0)
+{
+    Console.WriteLine(count);
+    count ++;
+}
+else
+{
+    count ++;
+}
+}
