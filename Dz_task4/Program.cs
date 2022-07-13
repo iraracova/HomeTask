8c1d4eafa63d9d670509fd66fa08e9afa8aebdb6
Ira Rakova <racova-ir@mail.ru>
2022-07-13 16:44:54 +0300

    add task4

---
+++ Dz_task4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите 3 разных числа:");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+
+if(a > max) max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+
+Console.Write("max = ");
+Console.WriteLine(max);
